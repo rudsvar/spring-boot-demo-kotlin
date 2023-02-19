6c2a0563cb0781e8f6f12d53570aad0b9221ca48
Rudi Blaha Svartveit <rudsvar@gmail.com>
2023-02-19 22:20:05 +0100

    Improve item tests

--- src/test/kotlin/com/example/demo/api/item/ItemControllerTest.kt
+++ src/test/kotlin/com/example/demo/api/item/ItemControllerTest.kt
@@ -19,6 +19,14 @@ import java.util.*
 @Transactional
 class ItemControllerTest(@Autowired val mockMvc: MockMvc, @Autowired val mapper: ObjectMapper) {
 
+    fun createNewItem(): ReadItem {
+        val name = UUID.randomUUID().toString()
+        val createItem = mapper.writeValueAsString(CreateItem(name, null))
+        val result = mockMvc.perform(post("/items").contentType(MediaType.APPLICATION_JSON).content(createItem))
+            .andReturn().response.contentAsString
+        return mapper.readValue(result, ReadItem::class.java)
+    }
+
     @Test
     fun createItemWorks() {
         val name = UUID.randomUUID().toString()
@@ -36,11 +44,7 @@ class ItemControllerTest(@Autowired val mockMvc: MockMvc, @Autowired val mapper:
 
     @Test
     fun getExistingItemGivesOk() {
-        val name = UUID.randomUUID().toString()
-        val createItem = mapper.writeValueAsString(CreateItem(name, null))
-        val result = mockMvc.perform(post("/items").contentType(MediaType.APPLICATION_JSON).content(createItem))
-            .andReturn().response.contentAsString
-        val readItem: ReadItem = mapper.readValue(result, ReadItem::class.java)
+        val readItem = createNewItem()
         mockMvc.perform(get("/items/" + readItem.id)).andExpect(status().isOk)
     }
 
@@ -54,11 +58,21 @@ class ItemControllerTest(@Autowired val mockMvc: MockMvc, @Autowired val mapper:
 
     @Test
     fun updateExistingItemGivesOk() {
+        val item = createNewItem()
         val name = UUID.randomUUID().toString()
-        val createItem = mapper.writeValueAsString(CreateItem(name, null))
-        val result = mockMvc.perform(post("/items").contentType(MediaType.APPLICATION_JSON).content(createItem))
-            .andReturn().response.contentAsString
-        val readItem: ReadItem = mapper.readValue(result, ReadItem::class.java)
-        mockMvc.perform(get("/items/" + readItem.id)).andExpect(status().isOk)
+        val update = mapper.writeValueAsString(CreateItem(name, null))
+        mockMvc.perform(put("/items/" + item.id).contentType(MediaType.APPLICATION_JSON).content(update))
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun deleteNonExistingItemGivesNotFound() {
+        mockMvc.perform(delete("/items/0")).andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun deleteExistingItemGivesOk() {
+        val item = createNewItem()
+        mockMvc.perform(delete("/items/" + item.id)).andExpect(status().isNoContent)
     }
 }
